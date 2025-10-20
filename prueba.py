a854f3197ea7b960431e0aa7cd1eaef9fabaadcc
Carlosromero779 <carlosromeros779@gmail.com>
2025-10-20 18:15:56 -0400

    first commit

---
+++ prueba.py
@@ -0,0 +1,20 @@
+print('hello esto funciona ahora modifica y agrega cualquier cosa aqui ')
+
+def suma(a, b):
+    return a + b
+def resta(a, b):
+    return a - b
+def multiplicacion(a, b):
+    return a * b
+def division(a, b):
+    return a / b
+
+resultado_suma = suma(5, 3)
+resultado_resta = resta(10, 4)
+resultado_multiplicacion = multiplicacion(6, 7)
+resultado_division = division(20, 5)
+
+print('Suma: ', resultado_suma)
+print('Resta: ', resultado_resta)
+print('Multiplicación: ', resultado_multiplicacion) 
+print('División: ', resultado_division)
